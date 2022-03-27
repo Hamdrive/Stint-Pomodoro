525d424acf9b908298b0e2ca2d7b979c1a859d03
Hamdrive <hamza.husein017@gmail.com>
2022-03-27 22:58:20 +0530

    [feat] add pomodoro component and route

---
+++ src/pages/Pomodoro/Pomodoro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from '../../components'
+
+export function Pomodoro() {
+  return (
+    <>
+        <Navbar />
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { Home } from "./Home/Home";
 export { Tasks } from "./Tasks/Tasks";
+export { Pomodoro } from "./Pomodoro/Pomodoro";
