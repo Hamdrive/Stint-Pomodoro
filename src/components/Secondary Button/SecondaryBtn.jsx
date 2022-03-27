c7d767aadbe8f2ea601954b52ae9d7d7598268a9
Hamdrive <hamza.husein017@gmail.com>
2022-03-27 22:21:40 +0530

    [feat] secondary btn component

---
+++ src/components/Secondary Button/SecondaryBtn.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./SecondaryBtn.module.css";
+
+export function SecondaryBtn({ disabled = false, id, callbackFn, children }) {
+  return (
+    <button
+      onClick={callbackFn}
+      id={id}
+      disabled={disabled}
+      className={`btn btn-md mx-sm ${styles.outline}`}
+    >
+      {children}
+    </button>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Modal } from "./Modal/Modal";
 export { PrimaryBtn } from "./Primary Button/PrimaryBtn";
+export { SecondaryBtn } from "./Secondary Button/SecondaryBtn";
