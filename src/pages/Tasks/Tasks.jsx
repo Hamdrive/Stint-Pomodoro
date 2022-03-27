d8b7c0e5911f8e88f90108dd7cb9790fdf749ca9
Hamdrive <hamza.husein017@gmail.com>
2022-03-27 12:35:10 +0530

    [feat] update text based on tasks

--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -3,49 +3,58 @@ import { Modal, Navbar } from "../../components";
 import styles from "./Tasks.module.css";
 
 export function Tasks() {
+  const [modal, setModal] = useState(false);
 
-    const [modal, setModal] = useState(false)
+  const [tasks, setTasks] = useState([]);
 
-    const [tasks, setTasks] = useState([])
-
-    const toggleModal = () => {
-        setModal(prev => !prev)
-    }
+  const toggleModal = () => {
+    setModal((prev) => !prev);
+  };
 
   return (
     <main className="">
       <Navbar />
-      <div className="main min-vh-85">
+      <div className="main container-height">
         <section className="max-width-1200 mx-auto px-md pos-rel">
-            {modal && <Modal toggleModal={toggleModal} setTasks={setTasks} />}
+          {modal && <Modal toggleModal={toggleModal} setTasks={setTasks} />}
           <div className="intro py-md">
             <h1 className="greeting txt-semibold">Welcome back, Hamza!</h1>
-            <h2 className="message txt-regular">
-              You have 3 tasks today! Go get em!
-            </h2>
+            {tasks.length === 0 ? (
+              <h2 className="message txt-regular">
+                No tasks! Great work!
+              </h2>
+            ) : (
+              <h2 className="message txt-regular">
+                You have {tasks.length} task(s) today! Go get em!
+              </h2>
+            )}
           </div>
-          <div className={`title-newtask ${styles.section} round-top-1 px-md`}>
+          <div className={`title-newtask ${styles.section} round-top-1 px-md tasks-height`}>
             <div className="flex-between py-md">
               <h2>Tasks To-Do</h2>
-              <button onClick={toggleModal} className={`btn btn-sec btn-fab ${styles.add__task}`}>
+              <button
+                onClick={toggleModal}
+                className={`btn btn-sec btn-fab ${styles.add__task}`}
+              >
                 +
               </button>
             </div>
             <div className="task-list">
-                {tasks.map((task) => (
-              <div className={`${styles.task} my-1 flex-between`}>
-                <p className="txt-md">{task.title}</p>
-                <div>
-                  <div onClick={toggleModal}
-                    className={`fas fa-edit fa-2x pointer ${styles.task__icon} `}
-                  ></div>
-                  <div onClick={toggleModal}
-                    className={`fas fa-trash fa-2x  pointer ${styles.task__icon} `}
-                  ></div>
+              {tasks.map((task) => (
+                <div className={`${styles.task} my-1 flex-between`}>
+                  <p className="txt-md">{task.title}</p>
+                  <div>
+                    <div
+                      onClick={toggleModal}
+                      className={`fas fa-edit fa-2x pointer ${styles.task__icon} `}
+                    ></div>
+                    <div
+                      onClick={toggleModal}
+                      className={`fas fa-trash fa-2x  pointer ${styles.task__icon} `}
+                    ></div>
+                  </div>
                 </div>
-              </div>
-
-                ))}
+              ))}
               {/* <div className={`${styles.task} my-1 flex-between`}>
                 <p className="txt-md">Task item</p>
                 <div>
