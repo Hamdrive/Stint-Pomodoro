62009defe65b22475fea84e49df3fb115b060a58
Hamdrive <hamza.husein017@gmail.com>
2022-03-31 13:38:12 +0530

    [update] handle theme for user system

--- src/context/theme-context.jsx
+++ src/context/theme-context.jsx
@@ -6,20 +6,26 @@ const useTheme = () => useContext(ThemeContext);
 
 const ThemeProvider = ({ children }) => {
   // fetches user system theme preference
-  const fetchLocalTheme = () =>
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const fetchLocalTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
   const [theme, setTheme] = useState(
-    JSON.parse(localStorage.getItem("userTheme"))
+    JSON.parse(localStorage.getItem("userTheme")) || false
   );
-
+  
+  // update theme
   const updateTheme = () => {
     setTheme((prevTheme) => !prevTheme);
   };
 
+  // store fetched theme of user system on initial render
   useEffect(() => {
-    localStorage.setItem("userTheme", fetchLocalTheme);
+    localStorage.setItem("userTheme", JSON.stringify(fetchLocalTheme));
   }, []);
+
+  // update localstorage theme
+  useEffect(() => {
+    localStorage.setItem("userTheme", JSON.stringify(theme));
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, updateTheme }}>
