780ba5ff9d210010d5273d7ee01635847b1d434c
Hamdrive <hamza.husein017@gmail.com>
2022-04-01 23:41:32 +0530

    [feat] implement 404 splashscreen

--- src/pages/404/InvalidRoute.jsx
+++ src/pages/404/InvalidRoute.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import InvalidRouteSplashscreen from "../../assets/404-Error.png";
+import { useTheme } from "../../context/theme-context";
+import styles from "./InvalidRoute.module.css";
 
 export function InvalidRoute() {
+  const { theme } = useTheme();
+
   return (
-    <div>404</div>
-  )
+    <div
+      className={` ${
+        theme ? "background__dark text__dark" : "background__light"
+      }  grid-container grid-1`}
+    >
+      <img
+        className={`${styles.splashscreen}`}
+        src={InvalidRouteSplashscreen}
+        alt={"404 error"}
+      />
+    </div>
+  );
 }
