f454d96683c00e666233cdea364f91b1fcbadfa8
Hamdrive <hamza.husein017@gmail.com>
2022-04-06 10:13:24 +0530

    [chore] cleanup unused imports

--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Helmet from "react-helmet";
 import { Modal } from "../../components";
 import { useTasks } from "../../context/tasks-context";
