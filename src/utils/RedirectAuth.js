8936619f250e64cd9a26fa564b327125b8cbfb04
Hamdrive <hamza.husein017@gmail.com>
2022-05-31 10:23:29 +0530

    [update] handle private routes

--- src/utils/RedirectAuth.js
+++ src/utils/RedirectAuth.js
@@ -1,5 +1,5 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/auth-context";
 
 export const RedirectAuth = () => {
   const {
